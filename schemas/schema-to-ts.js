6f9152f6f2abb9729544f913b361c44e49dd6fb9
Stefan Stoichev <stefan.stoichev@gmail.com>
2026-03-12 06:05:47 +0000

    schema to interface moved; task in package.json

--- schemas/schema-to-ts.js
+++ schemas/schema-to-ts.js
@@ -4,37 +4,37 @@ import { compileFromFile } from "json-schema-to-typescript";
 removeExistingFiles();
 
 await Promise.all([
-  compileFromFile("../../schemas/general.json", {
+  compileFromFile("./schemas/general.json", {
     customName: () => {
       return "GeneralConfig";
     },
   }).then((ts) => {
-    writeFileSync("../interfaces/general.d.ts", ts);
+    writeFileSync("./src/interfaces/general.d.ts", ts);
     console.log("General config interfaces were generated");
   }),
-  compileFromFile("../../schemas/qlik.json", {
+  compileFromFile("./schemas/qlik.json", {
     customName: () => {
       return "QlikConfig";
     },
   }).then((ts) => {
-    writeFileSync("../interfaces/qlik.d.ts", ts);
+    writeFileSync("./src/interfaces/qlik.d.ts", ts);
     console.log("Qlik config interfaces were generated");
   }),
-  compileFromFile("../../schemas/notifications.json", {
+  compileFromFile("./schemas/notifications.json", {
     customName: () => {
       return "NotificationsConfig";
     },
   }).then((ts) => {
-    writeFileSync("../interfaces/notifications.d.ts", ts);
+    writeFileSync("./src/interfaces/notifications.d.ts", ts);
     console.log("Notifications config interfaces were generated");
   }),
 ]);
 
 function removeExistingFiles() {
-  if (existsSync("../interfaces/general.d.ts"))
-    unlinkSync("../interfaces/general.d.ts");
-  if (existsSync("../interfaces/qlik.d.ts"))
-    unlinkSync("../interfaces/qlik.d.ts");
-  if (existsSync("../interfaces/notifications.d.ts"))
-    unlinkSync("../interfaces/notifications.d.ts");
+  if (existsSync("./src/interfaces/general.d.ts"))
+    unlinkSync("./src/interfaces/general.d.ts");
+  if (existsSync("./src/interfaces/qlik.d.ts"))
+    unlinkSync("./src/interfaces/qlik.d.ts");
+  if (existsSync("./src/interfaces/notifications.d.ts"))
+    unlinkSync("./src/interfaces/notifications.d.ts");
 }
